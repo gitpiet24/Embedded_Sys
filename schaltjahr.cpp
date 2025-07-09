81ae150858040b4801c988b850892391627a1f40
gitpiet24 <peterschwenk@gmx.de>
2025-07-09 07:22:38 +0000

    Init

---
+++ schaltjahr.cpp
@@ -0,0 +1,69 @@
+/*
+ * 1. Von der Problemanalyse zur Datendefinition:
+ *
+ * Identifiziere die Informationen, die dargestellt werden müssen, und lege fest
+ * wie sie in der gewählten Programmiersprache als Daten repräsentiert werden.
+ *
+ ** Eingabewert: "Jahreszahl" -> Nur positive ganzzahlige Zahlen
+ ** Ausgabewert: "IstSchaltjahr" -> Boolean "0": Kein Schaltjahr, "1" Schaltjahr 
+ */
+
+
+/*
+ * 2. Schnittstellendefinition:
+ *
+ * Ermittle welche Art von Daten die gewünschte Funktion verarbeitet und
+ * produziert.
+ ** siehe oben
+ *
+ * Formuliere eine prägnante Antwort auf die Frage, was die Funktion berechnet.
+ *
+ ** Funktion soll bewerten ob die eingegebene Jahreszahl ein Schaltjahr ist oder nicht.
+ ** 
+
+
+ * Welche Wertebereiche der Ein- und Ausgaben sind gültig, welche ungültig und
+ * wie verhält sich die Funktion bei ungültiger Eingabe?
+ *
+ * Definiere ein Funktionsgerüst, das zur Signatur passt.
+ *
+ */
+
+/*
+ * 3. Funktionale Beispiele:
+ *
+ * Erarbeite konkrete Beispiele für Funktions-Ein- und Ausgaben, die den Zweck
+ * der Funktion verdeutlichen.
+ *
+ */
+
+/*
+ * 4. Funktionsvorlage:
+ *
+ * Übertrage die Datendefinitionen in einen Entwurf der Funktion, der zunächst
+ * lediglich einen trivialen Wert zurückliefert.
+ *
+ */
+
+/*
+ * 5. Funktionsdefinition:
+ *
+ * Fülle die Lücken in der Funktionsvorlage aus.
+ *
+ * Nutze dabei die Zweckdefinition und die Beispiele.
+ *
+ */
+
+/*
+ * 6. Testen:
+ *
+ * Formuliere die Beispiele als Testfälle und stelle sicher, dass die Funktion
+ * alle Tests besteht.
+ *
+ * So entdeckst du Fehler.
+ *
+ */
+
+int main() {
+  return 0;
+}
